c75715287f55990c05f61d9901c1d5b5f207bf97
RichetinB <baptisterichetin@gmail.com>
2024-01-14 20:24:07 +0100

    ajustement

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -5,17 +5,14 @@ public class CanvasManager : MonoBehaviour
 {
     public void RestartGame()
     {
-        // Recharge la scène actuelle (recommence le jeu)
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
-        // Réactive le joueur et désactive le canvas Game Over
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         Canvas gameOverCanvas = GameObject.FindObjectOfType<Canvas>();
 
         if (player != null)
         {
             player.SetActive(true);
-            // Ajoute la ligne suivante pour activer le script MoveSpaceship
             player.GetComponent<MoveSpaceship>().enabled = true;
         }
 
