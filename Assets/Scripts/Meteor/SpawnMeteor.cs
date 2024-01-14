8fc36f09525299cd80314a7554330c147b2b991c
RichetinB <baptisterichetin@gmail.com>
2024-01-14 20:14:18 +0100

    reorganisation

--- Assets/Scripts/Meteor/SpawnMeteor.cs
+++ Assets/Scripts/Meteor/SpawnMeteor.cs
@@ -1,33 +1,20 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class SpawnMeteor : MonoBehaviour
 {
     [SerializeField] private float _maxTime = 1.5f;
     [SerializeField] private float _spawnHeight = 10f;
     [SerializeField] private GameObject _meteorPrefab;
-    [SerializeField] private Canvas _gameOverCanvas;
 
-    private float _timer;
-
-    private void Start()
+    private void OnEnable()
     {
-        Time.timeScale = 1.0f; // Assure que le temps n'est pas en pause au début
-        MeteorSpawn();
+        InvokeRepeating("MeteorSpawn", 0f, _maxTime);
     }
 
-    private void Update()
+    private void OnDisable()
     {
-        if (_timer > _maxTime)
-        {
-            MeteorSpawn();
-            _timer = 0;
-        }
-        _timer += Time.deltaTime;
-
-        CheckPlayerCollision();
+        CancelInvoke("MeteorSpawn");
     }
 
     private void MeteorSpawn()
@@ -36,31 +23,5 @@ public class SpawnMeteor : MonoBehaviour
         Vector3 spawnPos = new Vector3(randomX, _spawnHeight, 0f);
 
         GameObject meteor = Instantiate(_meteorPrefab, spawnPos, Quaternion.identity);
-
-        Rigidbody2D rb = meteor.GetComponent<Rigidbody2D>();
-        if (rb == null)
-        {
-            rb = meteor.AddComponent<Rigidbody2D>();
-        }
-
-        rb.gravityScale = 1.0f;
-
-        Destroy(meteor, 10f);
-    }
-
-    private void CheckPlayerCollision()
-    {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 0.5f);
-        foreach (Collider2D collider in colliders)
-        {
-            if (collider.CompareTag("Meteor"))
-            {
-                _gameOverCanvas.gameObject.SetActive(true);
-                Time.timeScale = 0f; 
-                gameObject.SetActive(false);
-                enabled = false;
-                break;
-            }
-        }
     }
 }
--- Assets/Scripts/MoveSpaceship.cs
+++ Assets/Scripts/MoveSpaceship.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class MoveSpaceship : MonoBehaviour
 {
     private float speed = 10.0f;
-
     private Rigidbody2D rb;
-
     private Vector2 movementDirection;
+    [SerializeField] private Canvas _gameOverCanvas;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Meteor") && gameObject.activeSelf)
+        {
+            HandleMeteorCollision();
+        }
+    }
+
+    private void HandleMeteorCollision()
+    {
+        _gameOverCanvas.gameObject.SetActive(true);
+        Time.timeScale = 0f;
+        gameObject.SetActive(false);
+        enabled = false;
+    }
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-    }   
+    }
 
     void Update()
     {
